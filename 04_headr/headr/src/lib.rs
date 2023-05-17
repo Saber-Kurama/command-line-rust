a7a5a9326b4f994a219a2d1cdeb80995e4cad583
guochao <463561248@qq.com>
2023-05-17 14:12:03 +0800

    ✨ feat:  完成数字转换字符串

--- 04_headr/headr/src/lib.rs
+++ 04_headr/headr/src/lib.rs
@@ -30,3 +30,41 @@ pub fn run(options: Config) -> MyResult<()> {
     println!("{:#?}", options);
     Ok(())
 }
+
+fn parse_positive_int(val: &str) -> MyResult<usize> {
+    // let b = 1;
+    // fn a() -> i32 {
+    //     b
+    // }
+    match val.parse::<usize>() {
+        Ok(v) if v > 0 => Ok(v),
+        // _ => Err(val.into()),
+        // _ => Err(Into::into(val)),
+        _ => Err(From::from(val)),
+    }
+    // val.parse::<usize>().map_err(|x| val)
+    // unimplemented!()
+    // Ok(val.to)
+    // Ok(1)
+}
+
+#[test]
+fn test_parse_positive_int() {
+    let string = "hello".to_string();
+    let other_string = String::from("hello");
+    assert_eq!(string, other_string);
+    // 测试数字字符串3
+    let res = parse_positive_int("3");
+    assert!(res.is_ok());
+    assert_eq!(res.unwrap(), 3);
+
+    // 测试非数字字符串
+    let res = parse_positive_int("foo");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "foo".to_string());
+
+    // ”0“
+    let res = parse_positive_int("0");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "0".to_string());
+}
