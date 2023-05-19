df3ba966a2b8f432f1ccbedb43b106f183296081
guochao <463561248@qq.com>
2023-05-19 10:56:54 +0800

    ✨ feat:  处理参数

--- 04_headr/headr/src/lib.rs
+++ 04_headr/headr/src/lib.rs
@@ -1,29 +1,61 @@
-use std::error::Error;
+use std::{error::Error, format};
 
 use clap::{App, Arg};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 #[derive(Debug)]
 pub struct Config {
-    // files: Vec<String>,
-    // lines: usize,
-    // bytes: Option<usize>,
+    files: Vec<String>,
+    lines: usize,
+    bytes: Option<usize>,
 }
 
 pub fn get_args() -> MyResult<Config> {
-    let matches = App::new("headr")
+    let matches: clap::ArgMatches = App::new("headr")
         .version("0.1.0")
-        .author("saber")
+        .author("saber saber@qq.com")
         .about("Rust head")
+        .arg(
+            Arg::with_name("lines")
+                .short("n")
+                .long("lines")
+                .value_name("LINES")
+                .help("Number of lines")
+                .default_value("10"),
+        )
+        .arg(
+            Arg::with_name("bytes")
+                .short("c")
+                .long("bytes")
+                .value_name("BYTES")
+                .help("Number of bytes")
+                .takes_value(true)
+                .conflicts_with("lines"),
+        )
         .arg(
             Arg::with_name("files")
                 .value_name("FILES")
-                .help("Input Text")
-                // .required(true)
+                .help("Input file(s)")
+                .required(true)
+                .default_value("-")
                 .min_values(1),
         )
         .get_matches();
-    Ok(Config {})
+    let lines = matches
+        .value_of("lines")
+        .map(parse_positive_int)
+        .transpose()
+        .map_err(|e| format!("illegal line count -- {}", e))?;
+    let bytes = matches
+        .value_of("bytes")
+        .map(parse_positive_int)
+        .transpose()
+        .map_err(|e| format!("illegal byte count -- {}", e))?;
+    Ok(Config {
+        files: matches.values_of_lossy("files").unwrap(),
+        lines: lines.unwrap(),
+        bytes,
+    })
 }
 
 pub fn run(options: Config) -> MyResult<()> {
